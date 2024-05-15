d6d8a80025b357bbe7cf956a3f6621cffa3ee913
Zhicheng Liu <159071350+MIKAzhccnn@users.noreply.github.com>
2024-05-15 18:13:21 -0400

    Update Q1.tsx

--- src/DetailQuestions/Q1.tsx
+++ src/DetailQuestions/Q1.tsx
@@ -1,49 +1,57 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useState, useContext } from "react";
 import { Form } from "react-bootstrap";
-import { AnswerContext } from "../AnswerContext"; // Make sure the path is correct
+import { AnswerContext } from "../AnswerContext";
 
-export function Q1(): JSX.Element {
-  const { userAnswers, setUserAnswers } = useContext(AnswerContext);
-  const reasons = [
+const reasons = [
     "Salary",
     "Work-life balance",
     "Helping others",
     "Others Reasons",
-  ];
+];
 
-  const [selectReason, setSelectReason] = useState<string>(
-    userAnswers[0] || "",
-  );
-  const [userInfo, setUserInfo] = useState<string>(userAnswers[1] || "");
+export function Q1():JSX.Element {
+    const [selectReason, setSelectReason] = useState<string | null>(null);
+    const [userInfo, setUserInfo] = useState<string>("");
+    const { userAnswers, setUserAnswers } = useContext(AnswerContext);
 
-  useEffect(() => {
-    setUserAnswers([selectReason, userInfo]);
-  }, [selectReason, userInfo, setUserAnswers]);
+    function updateReson(event: React.ChangeEvent<HTMLInputElement>){
+        setSelectReason(event.target.value)
 
-  function updateSelectReason(reason: string) {
-    setSelectReason(reason);
-  }
+    }
 
-  function updateUserInfo(event: React.ChangeEvent<HTMLInputElement>) {
-    setUserInfo(event.target.value);
-  }
+    function updateUserInfo(event: React.ChangeEvent<HTMLInputElement>) {
+        setUserInfo(event.target.value);
+        
+    }
 
-  return (
-    <div>
-      <h3>What is most important to you in a job?</h3>
-      {reasons.map((reason) => (
-        <Form.Check
-          key={reason}
-          type="radio"
-          label={reason}
-          name="reason-button"
-          checked={selectReason === reason}
-          onChange={() => updateSelectReason(reason)}
-        />
-      ))}
-      <br />
-      <p>If "Others" OR have anything to share, please indicate here:</p>
-      <Form.Control type="textbox" value={userInfo} onChange={updateUserInfo} />
-    </div>
-  );
+    return (
+        <div>
+            What is most important to you in a job?
+            <br></br>
+            <br></br>
+            <br></br>
+            {reasons.map((reason) => (
+                <Form.Check
+                    inline
+                    type="checkbox"
+                    label={reason}
+                    name="reason-button"
+                    checked={selectReason === reason}
+                    onChange={()=>setSelectReason(reason)}
+                />
+            ))}
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            if "Others" OR have anything to share, please indicate here:
+            <br></br>
+            <br></br>
+            <Form.Control
+                        type="textbox"
+                        value={userInfo}
+                        onChange={updateUserInfo}
+                    />
+        </div>
+    );
 }
